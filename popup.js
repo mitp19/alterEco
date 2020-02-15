4593cb92753a38620588224df4802c80a6d05604
Mit Patel <mitu.patel@mail.utoronto.ca>
2020-02-15 17:20:04 -0500

    adding starter files

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.getElementById("home").block.display = "hide";
+document.getElementById("insights").block.display = "hide";
+document.getElementById("rewards").block.display = "hide";
+console.log('getting here?')
