cdf4118a42356858d3593eb804ee7865e38e98b2
Mit Patel <mitu.patel@mail.utoronto.ca>
2020-02-17 18:11:55 -0500

    dynamic object scoring to carts added

--- cart.js
+++ cart.js
@@ -1,5 +1,6 @@
 console.log("CART JS");
 
+
 let tabUrl = window.location.toString()
 let regex = ".*amazon\.ca.*cart"
 if (tabUrl.match(regex)) {
@@ -13,17 +14,43 @@ if (tabUrl.match(regex)) {
     console.log(priceDiv);
     for (let item of priceDiv) {
         if (!item.className.includes("a-spacing-top-micro")) {
-            console.log("ITEM", item)
-            item.appendChild(createScoreBadge());
+            let itemDetails = item.parentNode.parentNode.parentNode;
+            let itemId = itemDetails.getAttribute('data-itemid');
+            console.log(itemId)
+            // console.log("PARET", item.parentNode.parentNode.parentNode)
+            item.appendChild(createScoreBadge(itemId));
         }
     }
 }
 
-function createScoreBadge() {
+function createScoreBadge(itemId) {
     let span = document.createElement("span");
-    span.className = "badge badge-pill badge-success";
-    span.appendChild(document.createTextNode(""))
+    let badgeColor = "success"
+    let score = getProductScore(itemId);
+    if (score >= 80) {badgeColor="success"} 
+    else if (score >= 60) {badgeColor="warning"} 
+    else {badgeColor="danger"} 
+
+    let scoreText = `alterEco Score ${score}`;
+    span.className = `badge badge-pill badge-${badgeColor}`;
+    span.appendChild(document.createTextNode(scoreText))
     return span;
     // <span class="badge badge-pill badge-primary">Primary</span>
 
+}
+
+function getProductScore(itemId) {
+    console.log("ITEMID", itemId);
+    let itemScoreMap = {
+        "C95b35eaf-2c48-4697-b43f-8556f12cb8c8": 17,
+        "Ca1824aec-1c2a-4e84-b8e6-d43a6b4a6eac": 72, 
+        "Cbdbd7706-f033-43a3-874e-7def3eba3eff": 41
+    }
+    if (itemId in itemScoreMap) {
+        console.log("FOUND KEY")
+        return itemScoreMap[itemId];
+    }
+    else {
+        return Math.floor(Math.random() * 100) + 1
+    }
 }
